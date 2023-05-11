fe80eaa937687550f203e1e711073a76b4f73f80
Abrotello <105622341+Abrotello@users.noreply.github.com>
2023-05-11 11:56:54 -0600

    Add files via upload

--- login.php
+++ login.php
@@ -1,47 +1,58 @@
 <?php
 
-include("conexion_BD.php");
+$nameserver = "localhost";
+$username = "adrian";
+$password = "12345";
+$database = "usuarios"; 
 
-$nombre = $_POST["name"];
-$password = $_POST["contrasena"];
+$conexion = mysqli_connect($nameserver, $username, $password, $database);
 
-//Login
-if(isset($_POST["btnIngresar"])) {
-
-    echo "Se presiono el boton";
+f(!$conexion) {
+    die("Conexion fallidaa ".mysqli_connect_error());
+} else {
+    echo "Conexion con exito";
+    $nombre = $_POST["name"];
+    $password = $_POST["contrasena"];
     
-    $sql = msqli_query($conexion, "SELECT * FROM usuario WHERE usuario = '$nombre' AND contrasena = '$password'");
-    $nr = mysqli_num_rows($sql);
-
-    echo "Se hizo la consulta";
-
-    if($nr==1) {
-        echo "<script> alert('Bienvenido $nombre'); </script>";
-        header("Location:principal.html");
-    } else {
-        echo "<script> alert('Usuario no existe'); </script>";
-        header("Location:index.html");
-    }
-
-}
-
-$Name = $_POST['nombre'];
-$Apellidos = $_POST['apellidos'];
-$Usuario = $_POST['usuario'];
-$Passwd = $_POST['contra'];
-$Confirm_Passwd = $_POST['conf_contra'];
-
-
-//Registro
-if(isset($_POST["btnRegistrar"])) {
-    if($Passwd == $Confirm_Passwd) {
+    //Login
+    if(isset($_POST["btnIngresar"])) {
         
-        $sqlDatos = "INSERT INTO usuario(nombre,apellidos,usuario,contrasena,confirmar_contrasena) VALUES('$Name','$Apellidos','$Usuario','$Passwd','$Confirm_Passwd ')";
+        echo "Se presiono el boton";
+    
+        $sql = msqli_query($conexion, "SELECT * FROM usuario WHERE usuario = '$nombre' AND contrasena = '$password'");
+        $nr = mysqli_num_rows($sql);
 
-        if(mysqli_query($conexion,$sqlDatos)) {
-            echo "<script> alert('Usuario registrado'); window.location='index.html' </script>";
+        echo "Se hizo la consulta";
+
+        if($nr==1) {
+            echo "<script> alert('Bienvenido $nombre'); </script>";
+            header("Location:principal.html");
         } else {
-            echo "Error: ".$sql."<br>".mysql_error($conexion);
+            echo "<script> alert('Usuario no existe'); </script>";
+            header("Location:index.html");
+        }
+
+    }
+    
+
+    $Name = $_POST['nombre'];
+    $Apellidos = $_POST['apellidos'];
+    $Usuario = $_POST['usuario'];
+    $Passwd = $_POST['contra'];
+    $Confirm_Passwd = $_POST['conf_contra'];
+
+
+    //Registro
+    if(isset($_POST["btnRegistrar"])) {
+        if($Passwd == $Confirm_Passwd) {
+            
+            $sqlDatos = "INSERT INTO usuario(nombre,apellidos,usuario,contrasena,confirmar_contrasena) VALUES('$Name','$Apellidos','$Usuario','$Passwd','$Confirm_Passwd ')";
+
+            if(mysqli_query($conexion,$sqlDatos)) {
+                echo "<script> alert('Usuario registrado'); window.location='index.html' </script>";
+            } else {
+                echo "Error: ".$sql."<br>".mysql_error($conexion);
+            }
         }
     }
 }
